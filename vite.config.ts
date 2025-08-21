c750ef553ef4122c590e21d1589e8b307055a854
criesbeck <chris.riesbeck@gmail.com>
2025-08-21 11:00:03 -0500

    add jsdom add missing test type

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -12,4 +13,8 @@ export default defineConfig({
         plugins: ['babel-plugin-react-compiler'],
       }
     })],
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  }
 })
