6fcd66663f71564b1598ba44ae59d5176fe52dc0
criesbeck <chris.riesbeck@gmail.com>
2025-09-12 15:37:10 -0500

    package update

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ const App = () => {
   return (
     <div className="text-center">
       <header className="bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-[calc(10px_+_2vmin)] text-white">
-        <img src={reactLogo} className="h-[20vmin] pointer-events-none animate-logo-spin" alt="logo" />
-        <img src={viteLogo} className="h-[20vmin] pointer-events-none animate-logo-spin" alt="logo" />
+        <img src={reactLogo} className="h-[20vmin] pointer-events-none motion-safe:animate-logo-spin" alt="logo" />
+        <img src={viteLogo} className="h-[20vmin] pointer-events-none motion-safe:animate-logo-spin" alt="logo" />
         <p className="m-4">Hello React + Vite!</p>
         <p>
           <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded " onClick={() => setCount(count => count + 1)}>
