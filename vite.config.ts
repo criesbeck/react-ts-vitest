07e26ad1b987c94b854e6467142fd1ad664ca657
criesbeck <chris.riesbeck@gmail.com>
2026-03-30 11:00:14 -0500

    downgrade typescript and eslint for compatibility with typescript-eslint and eslint react hooks update vite config for vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
+// https://github.com/reactjs/react.dev/issues/8353
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      }
-    })],
+    react(),
+    babel({
+      plugins: ['babel-plugin-react-compiler'],
+    }),
+  ],
   test: {
     globals: true,
-    environment: 'jsdom'
-  }
+    environment: 'jsdom',
+    setupFiles: './vite-setup.ts',
+  },
 })
